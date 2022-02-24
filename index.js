87d309d0e557a1d6fc40ce2d5427488bb33d17f0
Dhara Kachhadiya <dhara.k@codesquote.com>
2022-02-24 16:54:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoUrl =
+  "mongodb+srv://evspoint:donttouch1807@evspoint.sgihu.mongodb.net/evspoint";
+
+const ownerToken = require("./middleware/ownerToken");
+
+const ownerRoutes = require("./routes/ownerapi");
+
+app.use(bodyParser.json());
+
+app.use(ownerRoutes);
+
+mongoose
+  .connect(mongoUrl, {})
+  .then(() => {
+    console.log("connection-successful");
+  })
+  .catch((err) => console.log("no connection"));
+
+app.get("/owner", ownerToken, (req, res) => {
+  res.send({
+    ownerId: req.owner._id,
+    firstName: req.owner.firstName,
+    lastName: req.owner.lastName,
+    email: req.owner.email,
+    contactNo: req.owner.contactNo,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
