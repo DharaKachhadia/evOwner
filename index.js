bf243260bacb690182c0e3c7ad2a9dc0b8166395
Dhara Kachhadiya <dhara.k@codesquote.com>
2022-03-03 12:45:16 +0530

    new

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(cors())
+const port = process.env.PORT || 4000;
 const mongoUrl ="mongodb+srv://evspoint:donttouch1807@evspoint.sgihu.mongodb.net/evspoint";
  
 const ownerToken = require("./middleware/ownerToken");
@@ -13,7 +14,13 @@ const ownerRoutes = require("./routes/ownerapi");
 app.use(bodyParser.json());
 
 app.use(ownerRoutes);
-app.use(cors())
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE")
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type, Accept")
+
+  next();
+})
 mongoose
   .connect(mongoUrl, {})
   .then(() => {
